64df13abfc62470e3fbcf320effb83d7be1fd1a3
Mario Rodarte <mariohrodarte@outlook.com>
2024-09-24 17:58:32 -0600

    Introducción a Laravel

---
+++ introLaravel/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    @vite(['resources/js/app.js'])
+    <title></title>
+  </head>
+  <body>
+    <h1>¡Hola mundo!</h1>
+  </body>
+</html>
